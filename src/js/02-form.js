8be19377213aa4142a19c5f1bbf9877b2e367363
dilaraydmr <dilaraaydemir1@gmail.com>
2025-03-23 22:51:54 +0300

    msg

---
+++ src/js/02-form.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.feedback-form');
+
+//input event
+form.addEventListener('input', event => {
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  console.log({
+    email: email,
+    message: message,
+  });
+  // feedback-form-state
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: email,
+      message: message,
+    })
+  );
+});
+
+const formDataLocal = JSON.parse(localStorage.getItem('feedback-form-state'));
+console.log(formDataLocal);
+console.log(typeof formDataLocal);
+form.elements.email.value = formDataLocal.email;
+form.elements.message.value = formDataLocal.message;
